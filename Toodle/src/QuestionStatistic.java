033da32df114b32463a87f480370d0ddf7bad4be
SimonPSYCHOKB <simonyoudont@yahoo.com>
2017-03-28 17:56:46 +0200

    Impresionante calculo de estadisticas

---
+++ Toodle/src/QuestionStatistic.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+public class QuestionStatistic {
+	
+	private int correctNumber=0;
+	private int wrongNumber=0;
+	private int blankNumber=0;
+	
+	private Question question;
+	private ArrayList<Answer> answers = new ArrayList<Answer>();
+	
+	private double mean=0;
+	
+	public QuestionStatistic (Question q, ArrayList<Answer> aGiven ){
+		question = q;
+		answers = aGiven;
+		int total=0;
+		int people=0;
+		
+		for(Answer a : aGiven){
+			if(a.isBlank()){
+				blankNumber++;
+			}
+			else if(question.checkIfCorrect(a)){
+				correctNumber++;
+			}else{
+				wrongNumber++;
+			}
+			
+			total+=a.getGrade();
+			people++;
+			
+		}//For answer
+		
+		//Compute average
+		if(people ==0){
+			mean =(0);
+		}else{
+			mean = (total/people);
+		}
+	}
+	
+	public int getCorrectNumber() {
+		return correctNumber;
+	}
+	public void setCorrectNumber(int correctNumber) {
+		this.correctNumber = correctNumber;
+	}
+	public int getWrongNumber() {
+		return wrongNumber;
+	}
+	public void setWrongNumber(int wrongNumber) {
+		this.wrongNumber = wrongNumber;
+	}
+	public int getBlankNumber() {
+		return blankNumber;
+	}
+	public void setBlankNumber(int blankNumber) {
+		this.blankNumber = blankNumber;
+	}
+	public ArrayList<Answer> getAnswers() {
+		return answers;
+	}
+	public void setAnswers(ArrayList<Answer> answers) {
+		this.answers = answers;
+	}
+
+	public double getMean() {
+		return mean;
+	}
+
+	
+	
+	
+
+}
--- Toodle/src/Statistic.java
+++ Toodle/src/Statistic.java
@@ -1,26 +1,79 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
 
 /**
- * @author blanca
+ * @author Simon,  blanca
  *
  */
 public class Statistic {
 
-	/**
-	 * 
-	 */
-	public Statistic() {
-		// TODO Auto-generated constructor stub
+	private double mean =0;
+	private Exercise test;
+	private ArrayList<QuestionStatistic> qestStat = new ArrayList<QuestionStatistic>();
+	private ArrayList <AnsweredTest> ansTests= new ArrayList<AnsweredTest>();
+	  
+	 
+	public Statistic(Exercise t) {
+		test= t;
+		
+		//Get Answered Tests for all Students in the course
+		Course c = t.getCourse();
+		for (Student s : c.getStudents()){
+			AnsweredTest at = s.getAnsweredTest(test);
+			if(at !=null){
+				ansTests.add(at);
+			}
+		}
+		
+		//Create QuestionStatistics for every question in the test
+		
+		//Get all the Questions. One QuestionStatistic per Question
+		for(Question q: test.getQuestions()){
+			ArrayList<Answer> answers = new ArrayList<Answer> ();
+		
+			//For every student that responded
+			for(AnsweredTest at : ansTests){
+				//Get the matching Answer
+				for(Answer a : at.getAnswers()){
+					if(a.getQuestion().equals(q)){
+						answers.add(a);
+						break;
+					}//if
+				}//Student
+				
+			}//Each question
+			
+			qestStat.add(new QuestionStatistic(q, answers));
+			
+		}//Questions
+		
+		int count=0;
+		int total=0;
+		for (QuestionStatistic qs: qestStat){
+			total+=qs.getMean();
+			count++;
+		}
+		
+		mean = total/count;
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
+
+
+
+
+	public Exercise getTest() {
+		return test;
+	}
+
+
+
+	public void setTest(Exercise test) {
+		this.test = test;
 	}
 
 }
