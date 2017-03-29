832c5341319ba644234154ccb454180bdaa2962f
blancams <blancamartinselgas@gmail.com>
2017-03-29 11:58:59 +0200

    Javadoc

--- Toodle/src/Teacher.java
+++ Toodle/src/Teacher.java
@@ -4,23 +4,31 @@
  */
 
 /**
- * @author Simon Valcarcel, Blanca Martin
+ * @author Simon Valcarcel
+ * @author Blanca Martin
+ * 
+ * This class contains the information of the teacher
  *
  */
 public class Teacher extends User{
-
-	/**
-	 * 
-	 */
 	
-	//Constructor
+	/**
+	 * Constructor
+	 * 
+	 * @param fname - String with the first name of the Teacher
+	 * @param lname - String with the last name of the Teacher
+	 * @param password - String with the password of the Teacher
+	 * @param email - String with the email of the Teacher
+	 */
 	public Teacher(String fname, String lname, String password, String email) {
 		super(fname, lname,  password, email);
 	}
 
 
 	/**
-	 * @param args
+	 * This method accepts a student in a course
+	 * @param s - Student to be accepted
+	 * @param c - Course in which the Student has been accepted
 	 */
 	public void acceptStudent(Student s, Course c) {
 		Course cRem= null;
@@ -37,6 +45,11 @@ public class Teacher extends User{
 		}
 	}
 	
+	/**
+	 * This method rejects a student from a course
+	 * @param s - Student to be rejected
+	 * @param c - Course from which the Student has been rejected
+	 */
 	public void rejectStudent(Student s, Course c) {
 		Course cRem= null;
 		for (Course ct: s.getPendingCourses()){
@@ -51,20 +64,22 @@ public class Teacher extends User{
 		}
 	}
 	
+	/**
+	 * This method readmits a student in a course
+	 * @param s - Student to be readmitted
+	 * @param c - Course in which the Student has been accepted
+	 */
 	public void readmitStudent(Student s, Course c){
 		s.acceptStudent(c);
 	}
 	
+	/**
+	 * This method expels a student from a course
+	 * @param s - Student to be accepted
+	 * @param c - Course from which the Student has been expelled
+	 */
 	public void expellStudent(Student s, Course c){
 		s.expellStudent(c);
-	}
-	
-	public void viewStatistics(){
-		//Algo de las stats
-	}
-	
-	public void sendNotification(){
-		//Algo de las notificaciones
 	}
 		
 
